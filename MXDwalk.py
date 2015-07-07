d97f4b3188c4806d0dc13eb70add02a5cd591375
Justin Johnson <authalic@gmail.com>
2015-07-07 00:01:55 -0600

    Began the top-level script

--- MXDwalk.py
+++ MXDwalk.py
@@ -1,17 +1,24 @@
 
 # Walk through a drive or directory, recusively.
-# produces a list of MXD files, with full path, and last-modified date
+# produces a dictionary of MXD files, with full path, and last-modified datestamp
 
 import os
 
-rootDir = r'Y:\maps'
+def mxdwalk(rootdir):
+    
+    # create empty dictionary
+    # key = full path and filename of mxd file
+    # value = timestamp last modification
+    
+    mxdfiles = {}  
+    
+    for (dirName, subdirList, fileList) in os.walk(rootdir):
+        for fname in fileList:
+            # test if a file is an .mxd
+            if os.path.splitext(fname)[1].lower() == ".mxd":
+                mxdfile = os.path.join(dirName, fname)
+                # append filename and timestamp to dictionary
+                # index 8 of os.stat(file) is the integer timestamp of last modification
+                mxdfiles[mxdfile] = os.stat(mxdfile)[8]
 
-mxdCount = 0
-
-for (dirName, subdirList, fileList) in os.walk(rootDir):
-    for fname in fileList:
-        if os.path.splitext(fname)[1] == ".mxd":
-            print(os.path.join(dirName, fname))
-            mxdCount+=1
-
-print("done: %i files found" % mxdCount)
+    return mxdfiles
---
+++ mapextents.py
@@ -0,0 +1,49 @@
+
+# top level
+
+# Search a path for all MXD files
+# Create a dictionary, containing:
+#    full path filename (string)
+#    timestamp of last modification (integer)
+#    coordinates of layout extent (list of doubles)
+#       reproject to WGS84 or include the CRS in the GeoJSON
+#    path to exported image (string)
+# Store the dictionary in some kind of persistent data structure (JSON, xml, or pickle)
+# for each run of the application...
+#    locate and list each mxd file
+#       if the file has not been modified since the last run, move to the next mxd file..
+#    open the file and export a JPEG of the layout to an images folder
+#    export the extent of the layout (JSON or a polygon shapefile)
+#    (re)export the layout and extent info to GeoJSON file
+
+import os
+import geojson # https://pypi.python.org/pypi/geojson/1.2.0
+
+rootdir = r"Y:\maps"
+
+# open the existing data file
+
+mxddata = open(r'mxddatafile.json', 'r')
+
+
+def mxdwalk(rootdir):
+    
+    # create empty dictionary
+    # key = full path and filename of mxd file
+    # value = timestamp last modification
+    
+    mxdfiles = {}  
+    
+    for (dirName, subdirList, fileList) in os.walk(rootdir):
+        for fname in fileList:
+            # test if a file is an .mxd
+            if os.path.splitext(fname)[1].lower() == ".mxd":
+                mxdfile = os.path.join(dirName, fname)
+                # append filename and timestamp to dictionary
+                # index 8 of os.stat(file) is the integer timestamp of last modification
+                mxdfiles[mxdfile] = os.stat(mxdfile)[8]
+
+    return mxdfiles
+
+
+
