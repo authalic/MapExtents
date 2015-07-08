896e8a0bc969f78c30add09e12a8cb2f528237d4
Justin Johnson <authalic@gmail.com>
2015-07-07 18:23:09 -0600

    Began implementing functions to reproject data frames from UTM coordinates to Decimal Degrees (for use in Google Map API)

--- extenttest.py
+++ extenttest.py
@@ -6,6 +6,9 @@ import arcpy.mapping as map
 
 mxd = map.MapDocument(r'C:\projects\arcpy\newpark.mxd')
 
+arcpy.env.workspace = r'C:\temp'
+
+
 # Export a JPEG of the layout
 # map.ExportToJPEG(mxd, r'C:\projects\arcpy\test.jpg', resolution=100)
 
@@ -18,6 +21,14 @@ fs = arcpy.FeatureSet()
 # print the extent Geometry object in GeoJSON as a 2D envelope
 # http://resources.arcgis.com/en/help/rest/apiref/geometry.html
 
+# Extent of the DataFrame is exported in the coordinate system of the DataFrame
+# This will need to be reprojected to NAD83 dec.deg. for Google Maps API 
 
 for df in map.ListDataFrames(mxd):
     print df.extent.JSON
+    newdf = df.extent.projectAs(arcpy.SpatialReference(3857))  # WKID 3857: WGS 1984 - Web Mercator Aux Sphere
+    print newdf.JSON  # Error:  Prints in units of Meters, not DD.DDDD
+    
+    
+    
+    
