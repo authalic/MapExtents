874f2455b329466ebe0cfb8867da6179294a95ac
Justin Johnson <authalic@gmail.com>
2016-02-24 13:45:07 -0700

    Added timestamp info to the output JSON

--- mapextents.py
+++ mapextents.py
@@ -1,33 +1,40 @@
-# top-level file
-#
-# This will be the final working application
-# Code developed in other files will be incorporated into this file as it is completed
 
 import os
 import os.path
-import time
 import arcpy
 import arcpy.mapping
 import json
 import geojson # https://pypi.python.org/pypi/geojson/1.2.0
-
+import time
+import datetime
 from arcpy import env
 
 print "working"
 
 rootdir = r"M:\TenantAerials\ArcMap"
+# rootdir = r"C:\test\input"
 JPEGpath = r"C:\test\output"
 PDFpath = r"C:\test\output"
 
-JSONfile = r"C:\test\output\extents.json"
-filedictionary = r"C:\test\output\filedict.json"
+JSONoutfile = r"C:\test\output\extents.json"
+
+datadict = {}
+
+# check if the JSON file exists
+if os.path.exists(JSONoutfile):
+    # read the current contents into the dictionary
+    datadict = json.load(open(JSONoutfile, 'r'))
+
+# sample code:  Get all of the map names from the datadict
+#for x in datadict['features']:
+#    print x['properties']['map']
 
 
 def mxdwalk(rootdir):
     
-    # create empty dictionary
+    # returns a dictionary of MXD filenames and timestamps
     # key = full path and filename of mxd file
-    # value = timestamp last modification
+    # value = timestamp of last modification
     
     mxdfiles = {}  
     
@@ -36,14 +43,14 @@ def mxdwalk(rootdir):
             # test if a file is an .mxd
             if os.path.splitext(fname)[1].lower() == ".mxd":
                 mxdfile = os.path.join(dirName, fname)
-                # append filename and timestamp to dictionary
-                # index 8 of os.stat(file) is the integer timestamp of last modification
-                mxdfiles[mxdfile] = os.stat(mxdfile)[8]
+                # append filename and timestamp to dictionary as key
+                # add the integer timestamp of last modification as value
+                mxdfiles[mxdfile] = os.stat(mxdfile).st_mtime
 
     return mxdfiles
 
 
-def GeoJSONfeature(extent, mapname):
+def GeoJSONfeature(extent, mapname, timestamp):
     "Converts Map Document Dataframe Extent object to a GeoJSON Polygon Feature"
     
     # build tuples of each corner point (x, y)
@@ -57,13 +64,21 @@ def GeoJSONfeature(extent, mapname):
     
     # build the GeoJSON Feature using the Polygon
     # add any additional Properties here
-    fp = geojson.Feature(geometry=pg, properties={"map": mapname})
-    
+
+    datestamptext = datetime.date.fromtimestamp(timestamp)
+    print "datestamp:", datestamptext
+
+    # PROBLEM HERE
+    # The timetext variable might need to be converted to a string
+    # if it's some kind of Date object, it's not capable of being serialized into a JSON file
+    # try converting it to a String
+
+    fp = geojson.Feature(geometry=pg, properties=dict(map=mapname, modified=timestamp, timetext=str(datetime.date.fromtimestamp(timestamp))))
+
+    print fp
+
     return fp
 
-
-
-########  START THE FUN  ########
 
 # Get all MXD files in the root directory and its subdirectories
 mxdDictionary = mxdwalk(rootdir)
@@ -73,8 +88,10 @@ mxdPaths = mxdDictionary.keys()
 mxdPaths.sort()
 
 # Loop through all of the MXD files
+
 # future:  Check an external data file to see if the timestamp has changed since the previous run
-# Export a JPEG of the Layout
+
+# Export a JPEG and PDF of the Layout
 # Extract the Extent of each Data Frame in the Map Document
 
 # create a list to store the extent geometries of each MXD as GeoJSON Features
@@ -88,6 +105,11 @@ for mxdPath in mxdPaths:
     PDFfilename  = os.path.splitext(os.path.split(mxdPath)[1])[0] + ".pdf"
 
     mapname = os.path.splitext(os.path.split(mxdPath)[1])[0]
+
+    timestamp = mxdDictionary[mxdPath]
+
+    print "timestamp:", timestamp
+
 
     # Get the map document
     mxd = arcpy.mapping.MapDocument(mxdPath)
@@ -118,20 +140,17 @@ for mxdPath in mxdPaths:
             dfextent = df.extent.projectAs(arcpy.SpatialReference(4326))  # WKID 4326: GCS_WGS_1984 (decimal degrees)
 
         # get a GeoJSON Polygon Feature using the dataframe extent
-        GeoFeature = GeoJSONfeature(dfextent, mapname)
+        GeoFeature = GeoJSONfeature(dfextent, mapname, timestamp)
 
         # append the geo GeoJSON polygon to the list of all GeoJSON extents
         geoJSONfeatures.append(GeoFeature)
     
     time.sleep(10)
 
-
 fc = geojson.FeatureCollection(geoJSONfeatures)
 dump = geojson.dumps(fc)
 
-with open(JSONfile, 'w') as JSONoutput:
+with open(JSONoutfile, 'w') as JSONoutput:
     JSONoutput.write(dump)
 
 print "done"
-
-
